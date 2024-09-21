ee83c8570ef0da92e12c631be6a31b97a61e097d
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-09-21 14:33:22 +0200

    Update index.js

--- chatApp-com/backend/index.js
+++ chatApp-com/backend/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+
 const app = express();
+
 dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-app.listen("3000", () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
