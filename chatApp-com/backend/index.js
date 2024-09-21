d60ca972df77e8cd6580f0e4a5b5dd8564c2bcee
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-09-21 13:57:58 +0200

    Update index.js

--- chatApp-com/backend/index.js
+++ chatApp-com/backend/index.js
@@ -1,5 +1,9 @@
 import express from "express";
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 app.listen("3000", () => {
   console.log("Server running on http://localhost:3000");
 });
