9f7a241e0dd4fc58257c2274fe36b7cff68b2872
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-09-21 15:13:50 +0200

    Create dbConnect.js

---
+++ chatApp-com/backend/DB/dbConnect.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_CONNECT),
+      console.log("MongoDB connected");
+  } catch (error) {
+    console.log(console.error);
+  }
+};
+export default dbConnect;
